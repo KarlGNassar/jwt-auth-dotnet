23b8a079a07d831b82a5445aba3db880bbe3e27d
KarlGNassar <kgnassar01@gmail.com>
2022-01-24 16:11:09 +0200

    user model + connection string

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JwtAuth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuth.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuth.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
